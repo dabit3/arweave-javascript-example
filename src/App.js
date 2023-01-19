4ecd19e8a4fbd09e2fd5e2a52236a46569ad8653
S David Kong <SDAVIDKONG@gmail.com>
2023-01-19 13:57:28 -0800

    changed config to use legacy react start script

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import './App.css';
-import { useState, useEffect } from 'react';
+import "./App.css";
+import { useState, useEffect } from "react";
 
-import Arweave from 'arweave';
+import Arweave from "arweave";
 
-let arweave
+let arweave;
 
 if (process.env.REACT_APP_WORKSPACE_URL) {
   /* if in gitpod */
-  let host = process.env.REACT_APP_WORKSPACE_URL.replace('https://', '')
+  let host = process.env.REACT_APP_WORKSPACE_URL.replace("https://", "");
   arweave = Arweave.init({
     host,
-    protocol: 'https'
-  })
+    protocol: "https",
+  });
 } else {
   /* localhost / Arlocal */
-  arweave = Arweave.init({})
-  
+  arweave = Arweave.init({ host: "arweave.net", protocol: "https" });
+
   /* to use mainnet */
   // const arweave = Arweave.init({
   //   host: 'arweave.net',
@@ -25,91 +25,93 @@ if (process.env.REACT_APP_WORKSPACE_URL) {
 }
 
 function App() {
-  const [state, setState] = useState('')
-  const [transactionId, setTransactionId] = useState('')
-  const [loadingState, setLoadingState] = useState('')
+  const [state, setState] = useState("");
+  const [transactionId, setTransactionId] = useState("");
+  const [loadingState, setLoadingState] = useState("");
 
   async function createTransaction() {
-    if (!state) return
+    if (!state) return;
     try {
-      const formData = state
-      setState('')
-      setLoadingState('sendingTransaction')
+      const formData = state;
+      setState("");
+      setLoadingState("sendingTransaction");
       let transaction = await arweave.createTransaction({ data: formData });
       await arweave.transactions.sign(transaction);
       let uploader = await arweave.transactions.getUploader(transaction);
 
       while (!uploader.isComplete) {
         await uploader.uploadChunk();
-        console.log(`${uploader.pctComplete}% complete, ${uploader.uploadedChunks}/${uploader.totalChunks}`);
+        console.log(
+          `${uploader.pctComplete}% complete, ${uploader.uploadedChunks}/${uploader.totalChunks}`
+        );
       }
-      setTransactionId(transaction.id)
-      setLoadingState('transactionSent')
-    } catch(err) {
-      console.log('error: ', err)
+      setTransactionId(transaction.id);
+      setLoadingState("transactionSent");
+    } catch (err) {
+      console.log("error: ", err);
     }
   }
 
   async function readFromArweave() {
-    arweave.transactions.getData(transactionId, {
-      decode: true, string: true
-    }).then(data => {
-      console.log('data: ', data)
-    })
+    arweave.transactions
+      .getData(transactionId, {
+        decode: true,
+        string: true,
+      })
+      .then((data) => {
+        console.log("data: ", data);
+      });
   }
 
-  if (loadingState === 'sendingTransaction') return (
-    <div className="container">
-       <p>Sending Transaction...</p>
-    </div>
-  )
+  if (loadingState === "sendingTransaction")
+    return (
+      <div className="container">
+        <p>Sending Transaction...</p>
+      </div>
+    );
 
   return (
     <div className="container">
-      <button
-        style={button}
-        onClick={createTransaction}
-      >Create Transaction</button>
+      <button style={button} onClick={createTransaction}>
+        Create Transaction
+      </button>
 
-      {
-        loadingState === 'transactionSent' && (
-          <button
-            style={button}
-            onClick={readFromArweave}
-          >Log Transaction Data</button>
-        )
-      }
+      {loadingState === "transactionSent" && (
+        <button style={button} onClick={readFromArweave}>
+          Log Transaction Data
+        </button>
+      )}
 
       <input
         style={input}
-        onChange={e => {
-          setState(e.target.value)
-          setLoadingState('')
+        onChange={(e) => {
+          setState(e.target.value);
+          setLoadingState("");
         }}
         placeholder="text"
         value={state}
       />
     </div>
-  )
+  );
 }
 
 const button = {
-  outline: 'none',
-  border: '1px solid black',
-  backgroundColor: 'white',
-  padding: '10px',
-  width: '200px',
+  outline: "none",
+  border: "1px solid black",
+  backgroundColor: "white",
+  padding: "10px",
+  width: "200px",
   marginBottom: 10,
-  cursor: 'pointer'
-}
+  cursor: "pointer",
+};
 
 const input = {
-  backgroundColor: '#ddd',
-  outline: 'none',
-  border: 'none',
-  width: '200px',
-  fontSize: '16px',
-  padding: '10px'
-}
+  backgroundColor: "#ddd",
+  outline: "none",
+  border: "none",
+  width: "200px",
+  fontSize: "16px",
+  padding: "10px",
+};
 
 export default App;
